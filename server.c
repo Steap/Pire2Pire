f566dcf128b6bbe58dd282f7bf5ab0ffa790be22
robertv <robertv@2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2>
2010-02-18 11:23:45 +0000

    Wrong file in last commit...
    git-svn-id: svn+ssh://ssh.enseirb.fr/net/i1/roelandt/svn/RE203@4 2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2

--- server.c
+++ server.c
@@ -8,15 +8,31 @@
 #include <string.h>
 #include <signal.h>
 
-#define PORT 2345
+#define PORT 2340
 #define NB_QUEUE 10
 #define BUF_SIZE 4
 
 int conn = -1, sd = -1;
 
+void kill_handler(int pid) {
+  (void)pid;
+  if(shutdown(sd, SHUT_RDWR) == -1) {
+    perror("shutdown 2");
+  }
+  if(close(sd) == -1) {
+    perror("close 2");
+  }
+  exit(EXIT_SUCCESS);
+}
+
 int main(void) {
-  int yes = -1;
+  int nbread = -1, nbwritten = -1, retwrite = -1, yes = -1;
+  bool quit = false;
   struct sockaddr_in sa;
+  char buf[BUF_SIZE];
+  pid_t pid = -1;
+
+  signal(SIGINT, kill_handler);
 
   sd = socket(AF_INET, SOCK_STREAM, 0);
   if(sd == -1) {
@@ -43,23 +59,39 @@ int main(void) {
     return EXIT_FAILURE;
   }
 
-  conn = accept(sd, NULL, NULL);
-  if(conn == -1) {
-    perror("conn");
-    return EXIT_FAILURE;
-  }
+ while(!quit) {
+    conn = accept(sd, NULL, NULL);
+    if(conn == -1) {
+      perror("conn");
+      return EXIT_FAILURE;
+    }
 
-  if(shutdown(conn, SHUT_RDWR) == -1) {
-    perror("shutdown conn");
-  }
-  if(shutdown(sd, SHUT_RDWR) == -1) {
-    perror("shutdown sd");
-  }
-  if(close(conn) == -1) {
-    perror("close conn");
-  }
-  if(close(sd) == -1) {
-    perror("close sd");
+    pid = fork();
+
+    switch(pid) {
+    case -1:
+      perror("fork");
+      return EXIT_FAILURE;
+    case 0:
+      do {
+        nbread = read(conn, buf, sizeof(buf));
+        if(nbread == -1) {
+          perror("read");
+          return EXIT_FAILURE;
+        }
+        nbwritten = 0;
+        while(nbwritten < nbread) {
+          retwrite = write(conn, buf + nbwritten, nbread - nbwritten);
+          if(retwrite == -1) {
+            perror("write");
+            return EXIT_FAILURE;
+          }
+          nbwritten += retwrite;
+        }
+      } while(nbread > 0);
+    default:
+      break;
+    }
   }
 
   return EXIT_SUCCESS;
