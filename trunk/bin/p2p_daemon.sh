c44e7e662bd72a491a1bcd14f103a9bdb1a5eb71
roelandt <roelandt@2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2>
2010-03-04 23:48:30 +0000

    Added a script allowing users to control the daemon.
    git-svn-id: svn+ssh://ssh.enseirb.fr/net/i1/roelandt/svn/RE203@35 2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2

---
+++ trunk/bin/p2p_daemon.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+SELF=$(cd $(dirname $0); pwd -P)/$(basename $0)
+P2P_DAEMON='./daemon'
+LOCK_FILE='/tmp/k'
+
+function usage {
+    echo "Usage :"
+    echo -n "$(cd $(dirname $0); pwd -P)/$(basename $0) "
+    echo "[start | stop | restart]"
+}
+
+function server_start {
+    if [ ! -x $P2P_DAEMON ]
+    then
+        echo 'The daemon does not exist !';
+        exit 1;
+    fi
+    if [ -f $LOCK_FILE ]
+    then
+        echo '[FAIL] It seems like the daemon is already running';
+        exit 1;
+    fi
+    $P2P_DAEMON
+    echo ' * Daemon started'
+}
+
+function server_stop {
+    if [ ! -r $LOCK_FILE ]
+    then
+        echo 'The server does not seem to be launched...';
+    else
+        pid=`cat ${LOCK_FILE}`;
+        echo " * Sending SIGTERM to process ${pid}";
+        kill -TERM `cat ${LOCK_FILE}`;
+        echo " * Sending SIGKILL to process ${pid}";
+        kill -KILL $pid;
+    fi
+}
+
+function server_restart {
+    echo ' * About to restart the daemon...'
+    $SELF stop && $SELF start; 
+}
+
+# Main
+
+case "${1:-''}" in
+    'restart')
+        server_restart;
+        ;;
+    'start')
+        server_start;
+        ;;
+    'stop')
+        server_stop; 
+        ;;
+    *)
+        usage;
+esac
+
+
