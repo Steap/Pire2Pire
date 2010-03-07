dee37b81f1abe1b5b6a01c0e16e117b1c35bea9c
roelandt <roelandt@2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2>
2010-03-07 04:07:58 +0000

    Added a client structure that might come in handy.
    git-svn-id: svn+ssh://ssh.enseirb.fr/net/i1/roelandt/svn/RE203@48 2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2

--- trunk/daemon/client_handler.c
+++ trunk/daemon/client_handler.c
@@ -18,6 +18,29 @@
 #define DBG                          0
 #define MAX_REQUESTS_REACHED        -1
 
+struct client {
+    int                   socket;
+    char                  *addr;
+};
+
+static struct client *client_new (int socket, char *addr) {
+    struct client *c;
+    c = malloc (sizeof (struct client));
+    if (c == NULL)
+        return NULL;
+    c->socket = socket;
+    c->addr   = strdup (addr);
+    return c;
+}
+
+static void client_free (struct client *c) {
+    if (!c)
+        return;
+    if (c->addr)
+        free (c->addr);
+    free (c);
+}
+
 /* Numbers of threads that have been initialized */
 static int       n_initialized_threads = 0;
 
@@ -82,93 +105,13 @@ find_next_available_thread () {
     return MAX_REQUESTS_REACHED;
 }
 
-
-#if 0
-void
-handle_request (int client_socket, struct sockaddr_in *client_sockaddr) {
-    log_success (log_file, "OKOKOKOK");
-//    log_file = fopen ("/tmp/a", "w");
-//    log_success (log_file, "handling");
-    int i, r;
-
-    /* The message typed by the user might be longer than BUFFSIZE */
-    char    *message;
-    /* Number of chars written in message */ 
-    int     ptr = 0;
-    char    buffer[BUFFSIZE];
-    int     n_received;
-#if 1
-    log_success (log_file, "Connection from %s at ", 
-                           inet_ntoa (client_sockaddr->sin_addr));
-#endif
-    /* BUFFSIZE + 1, so the terminating null byte ('\0') can be stored */
-    if ((message = calloc (BUFFSIZE + 1, sizeof (char))) == NULL) {
-        perror ("Allocating memory for message");
-        return;
-    }
-    
-    while ((n_received = recv (client_socket, buffer, BUFFSIZE, 0)) != 0)
-    {
-        if (n_received < 0) {
-            perror ("recv");
-            // TODO: warn someone
-            return;
-        }
-        /*
-         * Seems necessary, unless we can guarantee that we will only receive
-         * well-formed strings... 
-         */
-        buffer[n_received] = '\0';
-
-        strcpy (message + ptr, buffer);
-        if((message = realloc (message, ptr + 2*BUFFSIZE + 1)) == NULL) {
-            fprintf (stderr, "Error reallocating memory for message\n");
-            // TODO: warn someone
-            return;
-        }
-        ptr += n_received;
-        message[ptr] = '\0';
-
-        if (strstr (buffer, "\n") != NULL) {
-
-            if ((message = realloc (message, BUFFSIZE+1)) == NULL) {
-                fprintf (stderr, "Error reallocating memory for message");
-                // TODO: warn someone
-                return;
-            }
-            ptr = 0;
-            break;
-        }
-    }
-
-
-    i = find_next_available_thread (); 
-    if (i == MAX_REQUESTS_REACHED) {
-        log_failure (log_file, " [Thread] Max threads reached !\n");
-        return;
-    }
-
-    r = pthread_create (threads+i, NULL, start, message);
-    if (r < 0) {
-        log_failure (log_file, "Could not start new thread");
-        return;
-    }
-
-    free (message);
-}
-#endif
 /*
- * Creates a new thread that handles the damn client.
+ * Handle requests by creating a new thread for each and every one of them.
  */
-void*
-foo (void *a) {
-    (void) a;
-    pthread_detach (pthread_self ());
-    return NULL;
-}
 static void*
-handle_request (void *arg) {
-    int client_socket = *((int *) arg);
+handle_requests (void *arg) {
+//    int client_socket = *((int *) arg);
+    struct client *client = (struct client *) arg;
     int i, r;
     /* The message typed by the user might be longer than BUFFSIZE */
     char    *message;
@@ -184,7 +127,7 @@ handle_request (void *arg) {
     }
    
     for (;;)  {
-        while ((n_received = recv (client_socket, buffer, BUFFSIZE, 0)) != 0)
+        while ((n_received = recv (client->socket, buffer, BUFFSIZE, 0)) != 0)
         {
             if (n_received < 0) {
                 perror ("recv");
@@ -242,9 +185,11 @@ handle_request (void *arg) {
     }
 
 out:
-    log_success (log_file, "End of !");
+    log_success (log_file, "End of %s", client->addr);
     if (message)
         free (message);
+    if (client)
+        client_free (client);
     pthread_detach (pthread_self ());
     return NULL;
 }
@@ -259,13 +204,17 @@ handle_client (int client_socket, struct sockaddr_in *client_addr) {
     log_success (log_file, "New client : %s", addr);
 
     i = find_next_available_thread (); 
-
     if (i == MAX_REQUESTS_REACHED) {
         log_failure (log_file, "==> COuld not handle another client");
         return;
     } 
 
-    r = pthread_create (threads+i, NULL, handle_request, &client_socket);
+    struct client *client;
+    client = client_new (client_socket, addr);
+    if (!client)
+        return;
+        
+    r = pthread_create (threads+i, NULL, handle_request, client);
     if (r < 0) {
         log_failure (log_file, "==> Could not create thread");
         return;
