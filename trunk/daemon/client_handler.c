d9da3af5aeae7e2f84be8c5e3fbbfa5a186e6bfa
roelandt <roelandt@2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2>
2010-03-07 04:41:35 +0000

    Cleaned up that shit so it's more readable.
    git-svn-id: svn+ssh://ssh.enseirb.fr/net/i1/roelandt/svn/RE203@49 2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2

--- trunk/daemon/client_handler.c
+++ trunk/daemon/client_handler.c
@@ -16,8 +16,66 @@
 
 #define BUFFSIZE                   128
 #define DBG                          0
-#define MAX_REQUESTS_REACHED        -1
+#define MAX_THREADS_REACHED        -1
 
+/****** Some shit that might end up in util/ *********/
+/* Given a socket, returns the message typed in */
+static char*
+get_request (int client_socket) {
+    /* The message typed by the user might be longer than BUFFSIZE */
+    char    *message;
+    /* Number of chars written in message */ 
+    int     ptr = 0;
+    char    buffer[BUFFSIZE];
+    int     n_received;
+
+    /* BUFFSIZE + 1, so the terminating null byte ('\0') can be stored */
+    if ((message = calloc (BUFFSIZE + 1, sizeof (char))) == NULL) {
+        perror ("Allocating memory for message");
+        return NULL;
+    }
+
+    while ((n_received = recv (client_socket, buffer, BUFFSIZE, 0)) != 0)
+    {
+        if (n_received < 0) {
+            perror ("recv");
+            // TODO: warn someone
+            return NULL;
+        }
+        /*
+         * Seems necessary, unless we can guarantee that we will only receive
+         * well-formed strings... 
+         */
+        buffer[n_received] = '\0';
+
+        strcpy (message + ptr, buffer);
+        if((message = realloc (message, ptr + 2*BUFFSIZE + 1)) == NULL) {
+            fprintf (stderr, "Error reallocating memory for message\n");
+            // TODO: warn someone
+            return NULL;
+        }
+        ptr += n_received;
+        message[ptr] = '\0';
+
+        if (strstr (buffer, "\n") != NULL) {
+
+            if ((message = realloc (message, BUFFSIZE+1)) == NULL) {
+                // TODO: warn someone
+                return NULL;
+            }
+            ptr = 0;
+            string_remove_trailer (message);
+            return message;
+        }
+    }
+    return NULL;
+}
+/**** End of the shit that might end up in util/ *****/
+
+
+
+
+/************** The client thingy *******************/
 struct client {
     int                   socket;
     char                  *addr;
@@ -41,6 +99,12 @@ static void client_free (struct client *c) {
     free (c);
 }
 
+/********** End of the client thingy ************/
+
+
+
+
+
 /* Numbers of threads that have been initialized */
 static int       n_initialized_threads = 0;
 
@@ -48,7 +112,7 @@ static int       n_initialized_threads = 0;
 extern FILE      *log_file;
 
 /* Might be a dummy implementation, we may want do something else one day */
-static pthread_t threads[MAX_REQUESTS];
+static pthread_t threads[MAX_THREADS];
 
 static int
 is_active_thread (pthread_t *t)
@@ -77,6 +141,10 @@ start (void *msg)
  * We cannot create a thread with an id that is already used by another thread.
  * Given that we use a table to store threads, we need to find the index
  * of the first available id.
+ *
+ * Be careful, it does return an index an not an address. That might be worth
+ * changing that one day, but eh, that ain't our biggest problem at the moment,
+ * right ?
  */
 static int
 find_next_available_thread () {
@@ -94,7 +162,7 @@ find_next_available_thread () {
         if (!is_active_thread (threads+i))
             return i;
 
-    if (i < MAX_REQUESTS) {
+    if (i < MAX_THREADS) {
         n_initialized_threads += 1;
 #if DBG
         fprintf (stdout, "i<MAX and n_init = %d\n", n_initialized_threads);
@@ -102,7 +170,7 @@ find_next_available_thread () {
         return i;
     }
     
-    return MAX_REQUESTS_REACHED;
+    return MAX_THREADS_REACHED;
 }
 
 /*
@@ -111,65 +179,28 @@ find_next_available_thread () {
 static void*
 handle_requests (void *arg) {
 //    int client_socket = *((int *) arg);
-    struct client *client = (struct client *) arg;
+    struct client *client;
     int i, r;
-    /* The message typed by the user might be longer than BUFFSIZE */
-    char    *message;
-    /* Number of chars written in message */ 
-    int     ptr = 0;
-    char    buffer[BUFFSIZE];
-    int     n_received;
+    /* The message typed by the user */
+    char    *message = NULL;
 
-    /* BUFFSIZE + 1, so the terminating null byte ('\0') can be stored */
-    if ((message = calloc (BUFFSIZE + 1, sizeof (char))) == NULL) {
-        perror ("Allocating memory for message");
-        return NULL;
-    }
-   
+    if (!(client = (struct client *) arg))
+        goto out;
+    
     for (;;)  {
-        while ((n_received = recv (client->socket, buffer, BUFFSIZE, 0)) != 0)
-        {
-            if (n_received < 0) {
-                perror ("recv");
-                // TODO: warn someone
-                goto out;
-            }
-            /*
-             * Seems necessary, unless we can guarantee that we will only receive
-             * well-formed strings... 
-             */
-            buffer[n_received] = '\0';
 
-            strcpy (message + ptr, buffer);
-            if((message = realloc (message, ptr + 2*BUFFSIZE + 1)) == NULL) {
-                fprintf (stderr, "Error reallocating memory for message\n");
-                // TODO: warn someone
-                goto out;
-            }
-            ptr += n_received;
-            message[ptr] = '\0';
+        message = get_request (client->socket);
+        if (!message)
+            goto out;
 
-            if (strstr (buffer, "\n") != NULL) {
-
-                if ((message = realloc (message, BUFFSIZE+1)) == NULL) {
-                    // TODO: warn someone
-                    goto out;
-                }
-                ptr = 0;
-                /**/
-                string_remove_trailer (message);
-                if (strcmp (message, "quit") == 0)
-                    goto out;
-                /**/
-                break;
-            }
-        }
-
+        if (strcmp (message, "quit") == 0)
+            goto out;
+        /* TODO else if, ..., else */
 
         i = find_next_available_thread (); 
-        if (i == MAX_REQUESTS_REACHED) {
+        if (i == MAX_THREADS_REACHED) {
             log_failure (log_file, " [Thread] Max threads reached !\n");
-            return NULL;
+            goto out;
         }
 
         /*
@@ -180,7 +211,7 @@ handle_requests (void *arg) {
         r = pthread_create (threads+i, NULL, start, message);
         if (r < 0) {
             log_failure (log_file, "Could not start new thread");
-            return NULL;
+            goto out;
         }
     }
 
@@ -196,27 +227,28 @@ out:
 
 void
 handle_client (int client_socket, struct sockaddr_in *client_addr) {
-    int i, r;
-    char *addr;
+    int             i, r;
+    char            *addr;
+    struct client   *client;
         
     addr = inet_ntoa (client_addr->sin_addr);
 
     log_success (log_file, "New client : %s", addr);
 
     i = find_next_available_thread (); 
-    if (i == MAX_REQUESTS_REACHED) {
+    if (i == MAX_THREADS_REACHED) {
         log_failure (log_file, "==> COuld not handle another client");
         return;
     } 
 
-    struct client *client;
     client = client_new (client_socket, addr);
     if (!client)
-        return;
-        
-    r = pthread_create (threads+i, NULL, handle_request, client);
+        return; 
+
+    r = pthread_create (threads+i, NULL, handle_requests, client);
     if (r < 0) {
         log_failure (log_file, "==> Could not create thread");
+        client_free (client);
         return;
     }
 }
--- trunk/daemon/client_handler.h
+++ trunk/daemon/client_handler.h
@@ -2,9 +2,7 @@
 #define CLIENT_HANDLER
 
 #include <netinet/in.h>
-#define MAX_REQUESTS  100
-
-//void handle_request (int client_socket, struct sockaddr_i
+#define MAX_THREADS  100
 
 /*
  * Takes care of the client in a new thread.
