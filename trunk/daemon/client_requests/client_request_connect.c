daad045f42f43d06056ea8976ec236d5aef8d52a
robertv <robertv@2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2>
2010-04-03 01:52:27 +0000

    Modified cr_connect so that you can't connect to yourself anymore. Replaced cmd by cmd_parse. Fixed error messages.
    git-svn-id: svn+ssh://ssh.enseirb.fr/net/i1/roelandt/svn/RE203@178 2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2

--- trunk/daemon/client_requests/client_request_connect.c
+++ trunk/daemon/client_requests/client_request_connect.c
@@ -6,16 +6,18 @@
 #include <stdio.h>              // printf ()
 #include <stdlib.h>             // NULL
 #include <string.h>             // strchr ()
+#include <unistd.h>             // close ()
 
 #include "../../util/logger.h"  // log_failure ()
 #include "../client.h"          // client_send ()
 #include "../client_request.h"  // struct client_request
 #include "../daemon.h"          // daemon_new ()
-#include "../util/cmd.h"        // struct option
+#include "../util/cmd_parser.h" // cmd_parse ()
 
-extern FILE* log_file;
-extern struct daemon *daemons;
-extern sem_t daemons_lock;
+extern FILE*            log_file;
+extern struct daemon    *daemons;
+extern sem_t            daemons_lock;
+extern char             my_ip[INET_ADDRSTRLEN];
 
 #define NOT_A_COUPLE           0
 #define NO_PORT_SPECIFIED     -1
@@ -23,31 +25,28 @@ extern sem_t daemons_lock;
 #define INVALID_PORT          -3
 #define INVALID_IP            -4
 
-static struct option options[] = {
-    {NULL, NULL, 0}
-};
-static int 
+static int
 is_valid_ip_port (char *foo, int *port)
 {
-    char *sep;
+    char *colon;
 
-    sep = strchr (foo, ':');
-    if (!sep)
+    colon = strchr (foo, ':');
+    if (!colon)
         return NOT_A_COUPLE;
 
-    if (strcmp (sep+1, "") == 0)
+    if (strcmp (colon + 1, "") == 0)
         return NO_PORT_SPECIFIED;
 
     /* Port */
     unsigned int i;
-    for (i = 0; i < strlen (sep+1); ++i)
-        if (!isdigit (sep[i+1]))
+    for (i = 0; i < strlen (colon+1); ++i)
+        if (!isdigit (colon[i+1]))
             return INVALID_PORT;
-                                                                                        
-    *port  = atoi (sep+1);
+
+    *port  = atoi (colon+1);
 
     /* IP */
-    *sep = '\0';
+    *colon = '\0';
     if (strlen (foo) == 0)
         return NO_IP_SPECIFIED;
 
@@ -58,96 +57,95 @@ is_valid_ip_port (char *foo, int *port)
 
 void*
 client_request_connect (void *arg) {
-    int                     argc;
-    char                    **argv;
-    int                     c;
-    int                     nb_arguments;
-    struct client_request   *r;
-
-#define MAX_ANSWER_SIZE 256
-
-    char                    answer[MAX_ANSWER_SIZE];
-    int                     daemon_socket;
+    struct client_request   *r = NULL;
+    char                    answer[256];
+    int                     daemon_socket = -1;
     struct sockaddr_in      daemon_addr;
-    struct daemon           *daemon;
+    struct daemon           *daemon = NULL;
+    struct parsed_cmd       *pcmd = NULL;
+    char                    *ip;
+    int                     port;
 
     r = (struct client_request *) arg;
     if (!r)
         return NULL;
-    argv = cmd_to_argc_argv (r->cmd, &argc);
-    while ((c = cmd_get_next_option (argc, argv, options)) > 0);
 
-    nb_arguments = argc - optind;
-    if (nb_arguments == 0) {
-        sprintf (answer, " < connect : you should specify an ip and a port.\n");
+    if (cmd_parse_failed ((pcmd = cmd_parse (r->cmd, NULL))))
+        return NULL;
+
+    if (pcmd->argc == 0) {
+        sprintf (answer, " < connect: Syntax is \"connect IP:PORT\"\n");
         goto send_msg;
     }
 
-    int port;
-    char *addr = argv[optind];
-    switch (is_valid_ip_port (addr, &port)) {
+    ip = pcmd->argv[1];
+    switch (is_valid_ip_port (ip, &port)) {
         case 1:
-            sprintf (answer, 
-                     " < connect : should connect to %s, port %d.\n",
-                     addr,
-                     port);
             break;
         case NOT_A_COUPLE:
-            sprintf (answer, " < You must specify an ip and a port.\n");
+            sprintf (answer,
+                    " < connect: you must specify an ip and a port\n");
             goto send_msg;
             break;
         case NO_PORT_SPECIFIED:
-            sprintf (answer, " < No port specified\n");
+            sprintf (answer, " < connect: no port specified\n");
             goto send_msg;
             break;
         case NO_IP_SPECIFIED:
-            sprintf (answer, " < No ip specified\n");
+            sprintf (answer, " < connect: no ip specified\n");
             goto send_msg;
             break;
         case INVALID_PORT:
-            sprintf (answer, " < invalid port\n");
+            sprintf (answer, " < connect: invalid port\n");
             goto send_msg;
             break;
         case INVALID_IP:
-            sprintf (answer, " < invalid ip\n");
+            sprintf (answer, " < connect: invalid ip\n");
             goto send_msg;
             break;
         default:
+            sprintf (answer, " < connect: unknown error\n");
             goto send_msg;
             break;
     }
 
-    /*
-     * TODO: We should not connect to ourselves, check the IP with my_ip
-     * and with 127.0.0.1 at least
-     */
+    if (strcmp (ip, my_ip) == 0
+        || strcmp (ip, "127.0.0.1") == 0) {
+        sprintf (answer, " < connect: can't connect to myself\n");
+        goto send_msg;
+    }
+
+    /* TODO: We should not connect to an already connected daemon */
+    /* TODO: Except if he has timed out ? :d */
 
     daemon_socket = socket (AF_INET, SOCK_STREAM, 0);
     if (daemon_socket < 0) {
         sprintf (answer,
-                 " < Could not open a new socket to %s. Error : %s\n",
-                 addr,
+                 " < connect: could not open a new socket to %s, error: %s\n",
+                 ip,
                  strerror (errno));
         goto send_msg;
     }
 
     // FIXME: inet_atop () ?
-    inet_aton (addr, &daemon_addr.sin_addr);
+    if (inet_pton (AF_INET, ip, &daemon_addr.sin_addr.s_addr) < 0) {
+        sprintf (answer, " < connect: couldn't parse IP %s\n", ip);
+    }
     daemon_addr.sin_port   = htons (port);
     daemon_addr.sin_family = AF_INET;
 
     if (connect (daemon_socket,
                 (struct sockaddr *) &daemon_addr,
-                sizeof (daemon_addr)) < 0) {    
-        sprintf (answer, 
-                 " < Could not connect to %s:%d.\n Error : %s.\n",
-                 addr, port, strerror (errno));
+                sizeof (daemon_addr)) < 0) {
+        sprintf (answer,
+                 " < connect: could not connect to %s:%d, error: %s\n",
+                 ip, port, strerror (errno));
         goto send_msg;
     }
 
-    daemon = daemon_new (daemon_socket, addr, port);
+    daemon = daemon_new (daemon_socket, ip, port);
     if (!daemon) {
-        sprintf (answer, " < Could not create a new daemon object");
+        sprintf (answer, " < connect: could not create a new daemon object");
         goto send_msg;
     }
 
@@ -155,16 +153,19 @@ client_request_connect (void *arg) {
     daemons = daemon_add (daemons, daemon);
     sem_post (&daemons_lock);
 
-  
-    sprintf (answer, " < Connected to %s. Everything went fine.\n", addr); 
-     
+
+    sprintf (answer,
+            " < connect: connected to %s successfully\n",
+            ip);
 
 send_msg:
     if (client_send (r->client, answer) < 0) {
-        log_failure (log_file, "do_set () : failed to send data to the client");
+        log_failure (log_file, "cr_connect: client_send () failed");
     }
-    
-    cmd_free (argv);
+    if (daemon_socket >= 0)
+        close (daemon_socket);
+    if (pcmd)
+        cmd_parse_free (pcmd);
 
     return NULL;
 }
