e1dff30f05bd07d6e963532cacfe47126cc57114
roelandt <roelandt@2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2>
2010-05-12 23:23:42 +0000

    Emptied the daemon_request_unknown function, so that the program does not crash when a daemon is trying to connect to another one using its client port.
    It is only a workaround.
    
    
    git-svn-id: svn+ssh://ssh.enseirb.fr/net/i1/roelandt/svn/RE203@206 2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2

--- branches/pooled/daemon/daemon_requests/daemon_request_unknown.c
+++ branches/pooled/daemon/daemon_requests/daemon_request_unknown.c
@@ -8,6 +8,25 @@ extern FILE *log_file;
 
 void*
 daemon_request_unknown (void* arg) {
+/*
+ * This currently does nothing, because of an annoying bug.
+ *
+ * Two machines : Zach & Lynette.
+ *
+ * Lynette tries to connect to Zach using its client port.
+ * Zach receives a "neighbour" command, but thinks it is a client command.
+ * Zach then sends back a "Unknwon command" message to Lynette, that is a client
+ * to him.
+ * Lynette receives this message, and since it is not a valid daemon command,
+ * sends back an "error" message.
+ * This goes on for ever, causing both daemon to crash.
+ *
+ * The easiest and dmbest solution is to never send error messages.
+ *
+ * FIXME : do something clever.
+ * Marked as Workaround. 
+ */
+#if 0
     struct daemon_request   *r;
     char                    answer[256];
 
@@ -15,13 +34,14 @@ daemon_request_unknown (void* arg) {
     if (!r)
         return NULL;
 
-    sprintf (answer, "error %s unknown command\n", r->cmd);
+    sprintf (answer, "error %s is an unknown command.\n\n", r->cmd);
+    log_send (log_file, answer);
     if (daemon_send (r->daemon, answer) < 0) {
         log_failure (log_file,
                      "do_unknown_command () : failed to send data back to the \
                      daemon");
         return NULL;
     }
-
+#endif
     return NULL;
 }
