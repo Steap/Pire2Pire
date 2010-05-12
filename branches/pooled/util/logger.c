e61f9abda42d8b4bf7970f3c12c7c8fc90008fe6
roelandt <roelandt@2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2>
2010-05-12 23:38:41 +0000

    Added a mutex to the logger. It should prevent different threads to try and write in our log files at the same time.
    git-svn-id: svn+ssh://ssh.enseirb.fr/net/i1/roelandt/svn/RE203@207 2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2

--- branches/pooled/util/logger.c
+++ branches/pooled/util/logger.c
@@ -1,23 +1,31 @@
+#include <pthread.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define ALL_MESSAGES    1
 
+
+static pthread_mutex_t lock_log = PTHREAD_MUTEX_INITIALIZER;
+
 static void
 logger (FILE *log, const char *msg, const char *header, va_list *arg) {
+    pthread_mutex_lock (&lock_log);
     fprintf (log, "%s", header);
     vfprintf (log, msg, *arg);
     fprintf (log, "\n");
     fflush (log);
+    pthread_mutex_unlock (&lock_log);
 }
 
 static void
 logger_no_newline (FILE *log, const char *msg,
                     const char *header, va_list *arg) {
+    pthread_mutex_lock (&lock_log);
     fprintf (log, "%s", header);
     vfprintf (log, msg, *arg);
     fflush (log);
+    pthread_mutex_unlock (&lock_log);
 }
 
 void
