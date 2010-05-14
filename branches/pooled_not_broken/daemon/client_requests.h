ca1d5562162917d12598232258ef34405a388ca1
roelandt <roelandt@2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2>
2010-05-14 16:10:51 +0000

    Pooled not broken in pooled_not_broken.
    git-svn-id: svn+ssh://ssh.enseirb.fr/net/i1/roelandt/svn/RE203@211 2111ffa0-b7a5-6d94-f42d-9f82cb6d3ce2

---
+++ branches/pooled_not_broken/daemon/client_requests.h
@@ -0,0 +1,14 @@
+#ifndef CLIENT_REQUESTS_H
+#define CLIENT_REQUESTS_H
+
+#include "client_requests/client_request_connect.h"
+#include "client_requests/client_request_download.h"
+#include "client_requests/client_request_get.h"
+#include "client_requests/client_request_help.h"
+#include "client_requests/client_request_info.h"
+#include "client_requests/client_request_list.h"
+#include "client_requests/client_request_raw.h"
+#include "client_requests/client_request_set.h"
+#include "client_requests/client_request_unknown.h"
+
+#endif
---
+++ branches/pooled_not_broken/daemon/daemon_requests/daemon_request_neighbour.c
@@ -0,0 +1,74 @@
+/*
+ * When receiving the "neighbour command" from another daemon, this daemon
+ * should try and add the given IP/port to his list of known daemons.
+ */
+#include <sys/socket.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../daemon.h"
+#include "../daemon_requests.h"
+
+#include "../../util/logger.h"
+
+
+extern sem_t                daemons_lock;
+extern struct daemon        *daemons;
+extern FILE                 *log_file;
+
+void*
+daemon_request_neighbour (void *arg) {
+    struct daemon_request   *r;
+    struct daemon           *tmp;
+    struct daemon           *new_daemon;
+    char                    *addr;
+    int                     port;
+    int                     new_daemon_socket;
+
+
+    r = (struct daemon_request *) arg;
+    if (!r) {
+        log_failure (log_file,
+                     "The argument of daemon_request_neighbour seems to be corrupted");
+        return NULL;
+    }
+
+
+    /*
+     * Ugly hack since we do not know what parser to use.
+     */
+    {
+        char *ipport;
+        ipport = strchr (r->cmd, ' ') +1;
+        port = atoi (1 + strchr (ipport, ':'));
+        *(strchr (ipport, ':')) = '\0';
+        addr = ipport;
+    }
+
+    sem_wait (&daemons_lock);
+    /* Checking whether the daemon is used */
+    for_each_daemon (tmp)
+        if (strcmp (tmp->addr, addr) == 0)
+            goto out;
+
+    /* Daemon is not already knwon : let's add it */
+    new_daemon_socket = socket (AF_INET, SOCK_STREAM, 0);
+    if (new_daemon_socket < 0) {
+        log_failure (log_file,
+                    "Could not add %s to the list of known daemons (%s)",
+                    addr,
+                    strerror (errno));
+        goto out;
+    }
+    new_daemon = daemon_new (new_daemon_socket, addr, port);
+    if (!new_daemon)
+        goto out;
+    daemons = daemon_add (daemons, new_daemon);
+
+out:
+    sem_post (&daemons_lock);
+    return NULL;
+}
